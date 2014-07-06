ba90216ea0bc3d18c1365efaf614294a106e1dd6
Thierry Nicolas <thierrynic@hotmail.com>
2014-07-05 20:41:10 -0400

    Added bootstrap and customizations

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Pinteresting02::Application.routes.draw do
   root "pages#home"
   get "about" =>"pages#about"
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
